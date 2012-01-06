ec4af1bd89d7e5b3de5d76aa463d177265e113db
iain.dillingham@gmail.com <iain.dillingham@gmail.com@55cda49a-89e6-7276-6960-440faabd67d8>
2012-01-06 15:08:24 +0000

    Added a collection to store a GeoMap's features.

--- trunk/GeoMap/src/org/gicentre/geomap/GeoMap.java
+++ trunk/GeoMap/src/org/gicentre/geomap/GeoMap.java
@@ -1,35 +1,37 @@
 package org.gicentre.geomap;
 
+import java.util.HashMap;
+import java.util.Map;
 import processing.core.PApplet;
 
 // *****************************************************************************************
-/** Class for drawing geographic maps in Processing 
+/** Class for drawing geographic maps in Processing
  *  @author Iain Dillingham and Jo Wood, giCentre, City University London.
- *  @version 1.0, 6th January, 2012 
- */ 
+ *  @version 1.0, 6th January, 2012
+ */
 // *****************************************************************************************
 
-/* This file is part of giCentre's geoMap library. geoMap is free software: you can 
+/* This file is part of giCentre's geoMap library. geoMap is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * geoMap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with this
- * source code (see COPYING.LESSER included with this source code). If not, see 
+ * source code (see COPYING.LESSER included with this source code). If not, see
  * http://www.gnu.org/licenses/.
  */
-
 public class GeoMap
 {
-	
-    private PApplet parent;									// The parent sketch.
-    private float minLon, maxLon;							// The minimum and maximum longitude values. These are converted to the x axis in screen coordinates.
-    private float minLat, maxLat;							// Minimum and maximum latitude values. These are converted to the y axis in screen coordinates.
-    private float xOrigin, yOrigin, mapWidth, mapHeight;	// The bounds of the map in screen coordinates.
+
+    private PApplet parent;                                 // The parent sketch.
+    private float minLon, maxLon;                           // The minimum and maximum longitude values. These are converted to the x axis in screen coordinates.
+    private float minLat, maxLat;                           // Minimum and maximum latitude values. These are converted to the y axis in screen coordinates.
+    private float xOrigin, yOrigin, mapWidth, mapHeight;    // The bounds of the map in screen coordinates.
+    private Map<Integer, Feature> features;                 // The key/value pair for each feature.
 
     /**
      * Constructs a map with the default bounds.
@@ -55,6 +57,7 @@ public class GeoMap
         this.yOrigin = yOrigin;
         this.mapWidth = mapWidth;
         this.mapHeight = mapHeight;
+        this.features = new HashMap<Integer, Feature>();
     }
 
     /**
@@ -71,6 +74,6 @@ public class GeoMap
      */
     public void draw()
     {
-    	// Do nothing for the moment.
+        // Do nothing for the moment.
     }
 }
