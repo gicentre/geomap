1ee88ecdc55f637f96190950eb3db32179f4512f
Jo Wood <jwoLondon@users.noreply.github.com>
2012-01-06 13:55:49 +0000

    Added LGPL reademe files and LGPL comment headers to each source file. Minor changes to header comments.

--- GeoMap/src/org/gicentre/geomap/Polygon.java
+++ GeoMap/src/org/gicentre/geomap/Polygon.java
@@ -7,23 +7,32 @@ import processing.core.PApplet;
 import processing.core.PMatrix2D;
 import processing.core.PVector;
 
-/**
- * Class to draw a polygon in screen coordinate space.
- * @author Jo Wood
- * @author Iain Dillingham
- * @version
+//*****************************************************************************************
+/** Class for drawing a polygon in screen coordinate space.
+ *  @author Jo Wood and Iain Dillingham, giCentre, City University London.
+ *  @version 1.0, 6th January, 2012 
+ */ 
+// *****************************************************************************************
+
+/* This file is part of giCentre's geoMap library. geoMap is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * geoMap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * source code (see COPYING.LESSER included with this source code). If not, see 
+ * http://www.gnu.org/licenses/.
  */
+
 public class Polygon
 {
 
-    /**
-     * The internal representation of the polygon's geometry.
-     */
-    private Path2D.Float path;
-    /**
-     * The parent sketch.
-     */
-    private PApplet parent;
+    private Path2D.Float path;		// Internal representation of the polygon's geometry.
+    private PApplet parent;			// Parent sketch.
 
     /**
      * Constructs a new polygon object with the default geometry.
